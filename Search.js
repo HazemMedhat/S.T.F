2d7ce6225af805a72c0138544c975c66900f5e60
HazemMedhat <102475810+HazemMedhat@users.noreply.github.com>
2023-04-17 13:31:07 +0200

    Update Search.js

--- Search.js
+++ Search.js
@@ -16,3 +16,106 @@ function myFunction() {
       }       
     }
   }
+
+
+// function filterTable() {
+//     // Get input element and filter value
+//     let input = document.getElementById("search-input");
+//     let filter = input.value.toUpperCase();
+    
+//     // Get table body
+//     let tableBody = document.querySelector(".ActiveStudents tbody");
+    
+//     // Get all rows of the table
+//     let rows = tableBody.getElementsByTagName("tr");
+    
+//     // Loop through all rows, hide those that don't match the search query
+//     for (let i = 0; i < rows.length; i++) {
+//       let cells = rows[i].getElementsByTagName("td");
+//       let rowMatches = false;
+//       for (let j = 0; j < cells.length; j++) {
+//         let cellText = cells[j].textContent.toUpperCase();
+//         if (cellText.indexOf(filter) > -1) {
+//           rowMatches = true;
+//           break;
+//         }
+//       }
+//       if (rowMatches) {
+//         rows[i].style.display = "";
+//       } else {
+//         rows[i].style.display = "none";
+//       }
+//     }
+//   }
+  
+//   // Listen for user input
+//   document.getElementById("search-input").addEventListener("input", filterTable);
+  
+
+
+const data = localStorage.getItem("allActiveStudents");
+   
+const students1 = JSON.parse(data);
+const createTableData1 = (students1) =>{
+    let html1 = ``
+    students1.forEach(student => {
+        html1 += `
+            <tr>
+                <td>${student.fullName1}</td>
+                <td>${student.sID}</td>
+                <td>${student.sEmail}</td>
+                <td>${student.sPhone}</td>
+                <td>${student.sGender}</td>
+                <td>${student.sLevel}</td>
+                <td>${student.sGpa}</td>
+                <td>${student.sDepartment}</td>
+                <td>${student.sBirthDate}</td>
+                <td>${student.sStatus}</td>
+                <td>${student.button}</td> 
+            </tr>
+        `
+    });
+    return html1;
+}
+const createTable1 = (students1) => {
+    return `
+        <table class="ActiveStudents">
+        <thead>
+        <tr>
+            <th scope="col">Name</th>
+            <th scope="col">ID</th>
+            <th scope="col">Email</th>
+            <th scope="col">Phone</th>
+            <th scope="col">Gender</th>
+            <th scope="col">Level</th>
+            <th scope="col">Gpa</th>
+            <th scope="col">Department</th>
+            <th scope="col">BirthDate</th>
+            <th scope="col">Status</th>
+            <th scope="col"> Assign Dep </th>
+        </tr>
+        </thead>
+        <tbody>
+        ${createTableData1(students1)}
+        </tbody>
+    </table>
+    `
+}
+document.getElementById('result').innerHTML = `
+    <div>
+        ${createTable1(students1)}
+    </div>
+`
+// function viewStudent(StudentInfo) {
+//     // Store the student data in localStorage
+//     localStorage.setItem('selectedStudent', JSON.stringify(StudentInfo));
+    
+//     // Redirect to the target page
+//     window.location.href = 'C:\Users\Hazem\Desktop\weeeeb\deprtement.html';
+//   }
+
+//   // Get the stored student data from localStorage
+// const selectedStudent = JSON.parse(localStorage.getItem('selectedStudent'));
+
+// // Do something with the student data, such as displaying it on the page
+// console.log(selectedStudent.fullName1);
