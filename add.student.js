68ad8f099108602758d98ecf5f46370c18d81341
Maryazuhair <102511269+Maryazuhair@users.noreply.github.com>
2023-04-17 20:24:57 +0200

    Update add.student.js

--- add.student.js
+++ add.student.js
@@ -1,4 +1,16 @@
 
+
+var levelField = document.getElementById("level");
+var departmentField = document.getElementById("department");
+
+levelField.onchange = function() {
+  if (levelField.value < 3) {
+    departmentField.disabled = true;
+  } else {
+    departmentField.disabled = false;
+  }
+};
+
 function AddLocal() {
   var fullName = document.getElementById("name").value;
   var ID = document.getElementById("id").value;
@@ -12,7 +24,7 @@ function AddLocal() {
   var Phone = document.getElementById("phone").value;
 
 var StudentInfo = {
-    firstName: fullName,
+    fullName1: fullName,
     sID: ID,
     sBirthDate: birthDate,
     sGpa: Gpa,
@@ -25,30 +37,82 @@ var StudentInfo = {
   };
 
  
-  if (StudentInfo.sStatus == "active") {
-    let activeStudentsArray =JSON.parse(localStorage.getItem("allActiveStudents")) || [];
-    activeStudentsArray.push(StudentInfo);
-    localStorage.setItem("allActiveStudents",JSON.stringify(activeStudentsArray));
-  } 
-  else 
-  {
-    let inactiveStudentsArray =JSON.parse(localStorage.getItem("allInActiveStudents")) || [];
-    inactiveStudentsArray.push(StudentInfo);
-    localStorage.setItem("allInActiveStudents",JSON.stringify(inactiveStudentsArray));
+  let allActiveStudents = JSON.parse(localStorage.getItem("allActiveStudents")) || [];
+  let allInActiveStudents = JSON.parse(localStorage.getItem("allInActiveStudents")) || [];
+  
+  let isActive = false;
+  let isInActive = false;
+  allActiveStudents.forEach(student => {
+    if (student.sID === ID) {
+      isActive = true;
+    }
+  });
+  allInActiveStudents.forEach(student => {
+    if (student.sID === ID) {
+      isInActive = true;
+    }
+  });
+
+  if (Status === "active") {
+    if (!isActive) {
+      allActiveStudents.push(StudentInfo);
+      localStorage.setItem("allActiveStudents", JSON.stringify(allActiveStudents));
+      return "Student added successfully!";
+    }
+    else {
+      return "The student already exists in the active list.";
+    }
+  } else {
+    if (!isInActive) {
+      allInActiveStudents.push(StudentInfo);
+      localStorage.setItem("allInActiveStudents", JSON.stringify(allInActiveStudents));
+      return "Student added successfully!";
+    }
+    else {
+      return "The student already exists in the inactive list.";
+    }
   }
+
 }
+
 function printMessage() {
-    let message = document.getElementById('message');
-    message.innerHTML='Student added successfully!';
+  let message = document.getElementById('message');
+  let errorMessage = AddLocal();
+  if(errorMessage === "Student added successfully!")
+  {
+    message.style.backgroundColor = 'green'; // set message color to red
+    message.innerHTML = errorMessage;
     message.removeAttribute('hidden');
     setTimeout(function () {
-        message.innerHTML='';
-        message.setAttribute('hidden', 'true');
-    }, 5000)
+      message.innerHTML='';
+      message.setAttribute('hidden', 'true');
+  }, 5000);
+  }
+  else if (errorMessage === "The student already exists in the active list.")
+  {
+    message.style.backgroundColor = 'red'; // set message color to red
+    message.innerHTML = errorMessage;
+    message.removeAttribute('hidden');
+    setTimeout(function () {
+      message.innerHTML='';
+      message.setAttribute('hidden', 'true');
+  }, 5000);
+  }
+  else if (errorMessage === "The student already exists in the inactive list.")
+  {
+    message.style.backgroundColor = 'red'; // set message color to red
+    message.innerHTML = errorMessage;
+    message.removeAttribute('hidden');
+    setTimeout(function () {
+      message.innerHTML='';
+      message.setAttribute('hidden', 'true');
+  }, 5000);
+  }
+  
 }
+
 let form = document.getElementById("form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  AddLocal();
   printMessage();
 });
